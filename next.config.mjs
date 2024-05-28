ca15847d45a3bede1b2c7bcfd749392fecbc0021
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-05-28 15:11:15 -0700

    Updated remotePatterns nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "images.ctfassets.net" }],
+    remotePatterns: [
+      { hostname: ["images.ctfassets.net", "images.unsplash.com"] },
+    ],
   },
 };
 
